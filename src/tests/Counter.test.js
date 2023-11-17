aa23b1008a5f81883b37dd5b04bbf8f398c5f9f0
neyaarajan <neyaarajan@ufl.edu>
2023-11-16 21:49:15 -0500

    initial commit

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,8 +1,9 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import { render, screen } from '@testing-library/react';
+import Counter from '../components/Counter';
 
 beforeEach(() => {
   // Render the Counter component here
+  render(<Counter />);
 })
 
 test('renders counter message', () => {
